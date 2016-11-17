93cd7f774fc8dbc5a80ea1043211a9d40c8d8d64
Spajkys <xsiska12@stud.fit.vutbr.cz>
2016-11-17 01:02:19 +0100

    Pridan testovaci script

---
+++ Test/test.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+cd Testy/
+
+FILES=`find . -type f -name \*` 
+
+cd ../..
+
+GREEN=$(tput setaf 2)
+NORMAL=$(tput sgr0)
+RED=$(tput setaf 1)
+
+for path in $FILES; do
+   path=`echo $path` 
+   path=`echo $path | sed 's/^.\//Test\/Testy\//g'` 
+   RET_CODE=`echo $path | grep -o -E '[0-9]+' | head -1 | sed -e 's/^0\+//'`
+   $r./prekladac $path >/dev/null 2>&1
+   ret=$?   
+   if [ $RET_CODE -eq $ret ]; then
+      printf "$path - ${GREEN}OK\n${NORMAL}"
+   else
+     printf  "$path ${RED}Ret code $ret - spravny $RET_CODE\n${NORMAL}" 
+   fi
+done
+
+
+
+ 
