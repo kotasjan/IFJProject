3cdb74c490dd36b5c4b3d4a8fb3cc6279a0df1fc
Kotas Jan <xkotas07@stud.fit.vutbr.cz>
2016-12-11 15:47:17 +0100

    Původní funkční testovací skript

--- test.sh
+++ test.sh
@@ -1,14 +1,41 @@
 #!/bin/sh
 
-cd testy/tests/
+GREEN=$(tput setaf 2)
+NORMAL=$(tput sgr0)
+RED=$(tput setaf 1)
+
+printf "\nPozitivni testy:\n\n"
+
+cd testy/pozitivni_testy/
 
 FILES=`find . -type f -name \*` 
 
 cd ../..
 
-GREEN=$(tput setaf 2)
-NORMAL=$(tput sgr0)
-RED=$(tput setaf 1)
+File="testy/vystup_pozitiv.txt"
+
+for path in $FILES; do
+   path=`echo $path` 
+   path=`echo $path | sed 's/^.\//testy\/pozitivni_testy\//g'` 
+   $r./prekladac $path >testy/vystup_pozitiv.txt 2>&1
+
+   if [ "$(cat "$File")" == "OK" ] ; then
+      printf "$path - ${GREEN}OK\n${NORMAL}"
+   else
+     printf  "$path - ${RED}NOT OK\n${NORMAL}" 
+   fi
+
+   > vystup_pozitiv.txt
+done
+
+
+printf "\nChybove testy:\n\n"
+
+cd testy/tests/
+
+FILES=`find . -type f -name \*` 
+
+cd ../..
 
 for path in $FILES; do
    path=`echo $path` 
@@ -23,6 +50,7 @@ for path in $FILES; do
    fi
 done
 
+printf "\nCizi testy:\n\n"
 
-
- 
+chmod +x testy/testy_cizi/ifj_tester-master/ifj_test.py
+python3 testy/testy_cizi/ifj_tester-master/ifj_test.py prekladac
